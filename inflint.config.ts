e4ce2afad56b92ce687f2044ca8967027ed22c53
Tal Rofe <tal@exlint.io>
2022-06-22 18:02:09 +0300

    refactor: 💡 upgrade inflint version
    upgrade inflint version

--- inflint.config.ts
+++ inflint.config.ts
@@ -1,8 +1,8 @@
-import { Config } from 'inflint';
+import { Config } from '@exlint.io/inflint';
 
 const inflintConfig: Config = {
 	rules: {
-		'{src}/**/*': [2, 'kebab-case.point'],
+		'src/**/*': [2, 'kebab-case.point'],
 		'**/*.yml': 2,
 	},
 };
